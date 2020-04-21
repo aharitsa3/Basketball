fdf934d7c8f36f972b29c1a67bd7390b9a5e7c6d
Ashwin Haritsa <aharitsa3@gatech.edu>
2020-04-21 17:32:05 -0400

    'update'

--- Scripts/test.R
+++ Scripts/test.R
@@ -1,18 +1,19 @@
 library(dplyr)
+library(ggplot2)
 
-TOR_roster_2019 <- read_csv("Desktop/basketballProject/TOR_roster_2019.csv")
-TOR_per_game_2019 <- read_csv("Desktop/basketballProject/TOR_per_game_2019.csv")
-team_stats_2019 <- read_csv("Desktop/basketballProject/team_stats_2019.csv", skip = 1) %>%
+team_stats_2019 <- read_csv("C:/Users/Ashwin's Computer/Desktop/Basketball/Data/team_stats_2019.csv", skip = 1) %>%
   mutate(Team = gsub('\\*','',Team))
 
 total_df = data.frame()
 
-for(team in c('TOR','ATL','LAL','OKC','MIA','HOU','GSW','PHO','CLE','POR')){
-  filename = paste0("Desktop/basketballProject/",team,"_team_totals_2019.csv")
+for(team in c('TOR','ATL','LAL','OKC','MIA','HOU','GSW','PHO','CLE','POR','UTA')){
+  # filename = paste0("Desktop/basketballProject/",team,"_team_totals_2019.csv")
+  filename = paste0("C:/Users/Ashwin's Computer/Desktop/Basketball/Data/",team,"_team_totals_2019.csv")
   team_totals = read_csv(filename) %>%
     rename(Player = X2) 
   
-  filename = paste0("Desktop/basketballProject/",team,"_roster_2019.csv")
+  # filename = paste0("Desktop/basketballProject/",team,"_roster_2019.csv")
+  filename = paste0("C:/Users/Ashwin's Computer/Desktop/Basketball/Data/",team,"_roster_2019.csv")
   roster = read_csv(filename)
 
   a <- team_totals %>% 
@@ -27,7 +28,7 @@ for(team in c('TOR','ATL','LAL','OKC','MIA','HOU','GSW','PHO','CLE','POR')){
   a <- a %>%
     merge(a %>% 
             group_by(Pos) %>%
-            summarise(Totals = n()), by='Pos')
+            summarise(NumPosition = n()), by='Pos')
   
   total_df <- rbind(total_df,a)
 }
@@ -38,4 +39,5 @@ test <- total_df %>%
   summarise(PER = sum(PER))
 
 
-barplot(test$PER,names.arg = test$Team)
+ggplot(data=test,aes(x=Team,y=PER)) +
+  geom_bar()
